8f62ffdfbb6fafcf8ffc7eb90323583aa28c4a84
aleksandr <4ymail@mail.ru>
2022-06-30 18:26:53 +0300

    *

---
+++ XOgame.Core/Migrations/20220630063435_ChangeRemoveRoom.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XOgame.Core.Migrations
+{
+    public partial class ChangeRemoveRoom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Rooms_RoomId",
+                table: "Games");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Rooms_RoomId",
+                table: "Games",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Rooms_RoomId",
+                table: "Games");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Rooms_RoomId",
+                table: "Games",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id");
+        }
+    }
+}
